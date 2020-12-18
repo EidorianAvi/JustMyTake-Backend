fefeefe28207ec96f2c48a74d0e4722a710a3f3c
Adrian <reneavila1993@gmail.com>
2020-12-18 16:45:53 -0700

    Add length validation on password and username

--- models/User.js
+++ models/User.js
@@ -4,11 +4,16 @@ const UserSchema = mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        minLength: [5, "Must be 5-10 chars"],
+        maxLength: [10, "Must be 5-10 chars"],
+
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minLength: [6, "Must be 6-10 chars"],
+        maxLength: [10, "Must be 6-10 chars"],
     },
     email: {
         type: String,
